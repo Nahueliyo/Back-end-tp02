fc19b986f6a0387903c1baa849b030f8aab198fd
Nahuel Castello <nahuel250797@gmail.com>
2022-11-08 15:51:45 -0300

    version1.0

---
+++ index.js
@@ -0,0 +1,15 @@
+function mostrar(param){
+    return 'a'
+}
+
+const mostrar2 = param => {
+    return 'a'
+}
+
+const mostrar3 = param => 'a'
+
+const sumar = (a, b) => a + b
+
+function sumar(a, b){
+    return a+b
+}
